72d3d0586e4b93fb42af9777dcaa5d3161c80076
JoiceStefany <steffanyjoice006@gmail.com>
2023-11-21 14:35:51 -0300

    Replacement DTO

---
+++ src/main/java/com/limajoice/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,21 @@
+package com.limajoice.dslist.dto;
+
+public class ReplacementDTO {
+    
+    private Integer sourceIndex;
+    private Integer destinationIndex;
+    
+    public Integer getSourceIndex() {
+        return sourceIndex;
+    }
+    public Integer getDestinationIndex() {
+        return destinationIndex;
+    }
+    public void setSourceIndex(Integer sourceIndex) {
+        this.sourceIndex = sourceIndex;
+    }
+    public void setDestinationIndex(Integer destinationIndex) {
+        this.destinationIndex = destinationIndex;
+    }
+
+}
