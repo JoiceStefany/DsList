9aaeb5e25fe0c739f0cd1f40bd72a0ac47043421
JoiceStefany <steffanyjoice006@gmail.com>
2023-11-21 14:59:27 -0300

    Move games on list

--- src/main/java/com/limajoice/dslist/controllers/GameListController.java
+++ src/main/java/com/limajoice/dslist/controllers/GameListController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.limajoice.dslist.dto.GameListDTO;
 import com.limajoice.dslist.dto.GameMinDTO;
+import com.limajoice.dslist.dto.ReplacementDTO;
 import com.limajoice.dslist.services.GameListService;
 import com.limajoice.dslist.services.GameService;
 
@@ -24,6 +27,11 @@ public class GameListController {
 	@Autowired
 	private GameService gameService;	
 
+	@GetMapping(value = "/{id}")
+	public GameListDTO findById(@PathVariable Long id){
+		GameListDTO result = gameListService.findById(id);
+		return result;
+	}
 	
 	@GetMapping
 	public List<GameListDTO> findAll(){
@@ -34,6 +42,11 @@ public class GameListController {
 	public List<GameMinDTO> findByList(@PathVariable Long listId){
 		List<GameMinDTO> result = gameService.findByList(listId);
 		return result;
+	}
+
+	@PostMapping(value = "/{listId}/replacement")
+	public void move(@PathVariable Long listId, @RequestBody ReplacementDTO body){
+		gameListService.move(listId, body.getSourceIndex(), body.getDestinationIndex());
 	}
 	
 }
