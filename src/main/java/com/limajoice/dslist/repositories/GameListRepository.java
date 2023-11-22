44d2bcfe31568a3aaecb9be80c77d64eb141cb79
JoiceStefany <steffanyjoice006@gmail.com>
2023-11-22 11:55:11 -0300

    Deploy ready

--- src/main/java/com/limajoice/dslist/repositories/GameListRepository.java
+++ src/main/java/com/limajoice/dslist/repositories/GameListRepository.java
@@ -1,9 +1,15 @@
 package com.limajoice.dslist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.limajoice.dslist.entities.GameList;
 
 public interface GameListRepository extends JpaRepository<GameList, Long>{
 
+    @Modifying
+    @Query(nativeQuery = true, 
+                value = "UPDATE tb_belongin SET position = :new Position WHERE list_id = :listId AND game_id = :gameId")
+                void updateBelogingPosition(Long listId, Long gameId, Integer newPosition);
 }
--- src/main/java/com/limajoice/dslist/services/GameListService.java
+++ src/main/java/com/limajoice/dslist/services/GameListService.java
@@ -10,7 +10,9 @@ import com.limajoice.dslist.dto.GameListDTO;
 
 
 import com.limajoice.dslist.entities.GameList;
+import com.limajoice.dslist.projections.GameMinProjection;
 import com.limajoice.dslist.repositories.GameListRepository;
+import com.limajoice.dslist.repositories.GameRepository;
 
 
 @Service
@@ -18,10 +20,35 @@ public class GameListService {
 	
 	@Autowired
 	private GameListRepository gameListRepository;
+
+	@Autowired
+	private GameRepository gameRepository;
 	
 	@Transactional(readOnly = true)
 	public List<GameListDTO> findAll() {
 		List<GameList> result = gameListRepository.findAll();
-		return result.stream().map(x -> new GameListDTO(x)).toList();
+		return result.stream().map(GameListDTO :: new).toList();
+	}
+
+	@Transactional
+	public void move (Long listId, int sourceIndex, int destinationIndex){
+
+		List <GameMinProjection> list = gameRepository.searchByList(listId);
+
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+		for (int i = min; i <= max; i++){
+				gameListRepository.updateBelogingPosition(listId, list.get(i).getId(), i);
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public GameListDTO findById(Long id){
+		GameList entity = gameListRepository.findById(id).get();
+		return new GameListDTO(entity);
 	}
 }
